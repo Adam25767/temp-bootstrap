b1805b580cd98bb5bd87166ab5bfeae616a61e56
Adam25767 <98131913+Adam25767@users.noreply.github.com>
2024-04-12 04:48:27 +0200

    Update MainActivity.kt

--- {{cookiecutter.out_dir}}/android/app/src/main/kotlin/{{ cookiecutter.kotlin_dir }}/MainActivity.kt
+++ {{cookiecutter.out_dir}}/android/app/src/main/kotlin/{{ cookiecutter.kotlin_dir }}/MainActivity.kt
@@ -1,6 +1,6 @@
 package {{ cookiecutter.org_name }}.{{ cookiecutter.project_name }}
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
